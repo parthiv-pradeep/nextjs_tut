8c4ed4c24f0e57c4d4f784be9e0b35983d49582a
parthiv-pradeep <parthivpradeep@mgmcet.ac.in>
2025-05-16 20:41:56 +0530

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function page() {
+    return(
+        <h1>customer page route</h1>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function page(){
+    return(
+        <h1>THis is invoice route page</h1>
+    )
+}
